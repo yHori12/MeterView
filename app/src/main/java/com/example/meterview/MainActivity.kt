ad02063fcfd901b4e21d1147ef347bcc6cce78b9
yHori12 <hori@yadokari.tv>
2020-01-02 17:17:45 +0900

    create customView

--- app/src/main/java/com/example/meterview/MainActivity.kt
+++ app/src/main/java/com/example/meterview/MainActivity.kt
@@ -1,10 +1,9 @@
 package com.example.meterview
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_main.*
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity(){
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
--- app/src/main/java/com/example/meterview/MeterView.kt
+++ app/src/main/java/com/example/meterview/MeterView.kt
@@ -21,31 +21,37 @@ class MeterView @JvmOverloads constructor(
     defStyle: Int = 0
 ) : View(context, attrs, defStyle) {
 
+    companion object {
+        private const val TIMER_VIBRATION = 5L
+        private const val NUM_LEVEL = 5
+        private const val SWIPE_MINIMUM_DISTANCE = 10
+    }
+
     private var mPreviousY = 0f
     private var mScreenHeight: Int = 0
     private var mScreenWidth: Int = 0
+
     private var vibrator: Vibrator =
         getContext().getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
-    private var mSwipeState = SWIPE_STATE.NONE
+    //draw用
+    private var mSwipeState = SwipeState.NONE
     private var mLevelButtonHeight = 50f
     private var mLevelButtonMargin = 35f
     private var connerRadius = 60
     private var mPaintStroke = 5f
     private var mLevelButtonDiffRatio = 0.5f
     private var mLevelButtonDifference = 35f
-    private val NUM_LEVEL = 5
 
     private val mMaxMeterLevel: Int
         get() {
             return NUM_LEVEL - 1
         }
-    private val TIMER_VIBRATION = 5L
-    private val SWIPE_MINIMUM_DISTANCE = 10
 
     private var mCurrentLevel: Int = 0
+
     private val mExtractLevels = mutableListOf<ExtractLevel>()
 
-    internal enum class SWIPE_STATE {
+    internal enum class SwipeState {
         UP,
         DOWN,
         NONE
@@ -58,26 +64,22 @@ class MeterView @JvmOverloads constructor(
 
     @SuppressLint("DrawAllocation")
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
-
-//        スクリーンサイズを取得する。
+        //スクリーンサイズを取得する。
         val displayMetrics = DisplayMetrics()
         (context.getSystemService(Context.WINDOW_SERVICE) as WindowManager).defaultDisplay.getMetrics(
             displayMetrics
         )
         mScreenWidth = displayMetrics.widthPixels
         mScreenHeight = displayMetrics.heightPixels
-//        widthMeasureSpec と heightMeasureSpec には親 View から『この View はどのようなサイズになってほしいか』という情報が入ってきます。
-//         値は int 型ですが、モード情報が上位 ２bit に、サイズ情報がそれ以外に入っています。MeasureSpec には３つのモードあります。
-//        MeasureSpec.UNSPECIFIED	00	特に何も指定しないモード
-//        MeasureSpec.EXACTLY	01	値を指定するモード
-//        MeasureSpec.AT_MOST	10	指定した値以下にするモード
+
+        //サイズの指定が会った際に大きさを取得する。
         if (MeasureSpec.UNSPECIFIED != MeasureSpec.getMode(widthMeasureSpec)) {
             mScreenWidth = MeasureSpec.getSize(widthMeasureSpec)
         }
         if (MeasureSpec.UNSPECIFIED != MeasureSpec.getMode(heightMeasureSpec)) {
             mScreenHeight = MeasureSpec.getSize(heightMeasureSpec)
         }
-//        自分自身のサイズを setMeasuredDimension で確定させる
+        //自分自身のサイズを setMeasuredDimension で確定させる
         setMeasuredDimension(mScreenWidth, mScreenHeight)
         initButtonSize(mScreenWidth, mScreenHeight)
     }
@@ -85,16 +87,17 @@ class MeterView @JvmOverloads constructor(
     private fun initButtonSize(mScreenWidth: Int, mScreenHeight: Int) {
         val maxBtnWidth = mScreenWidth.toFloat()
         val minBtnWidth = mScreenWidth * mLevelButtonDiffRatio
-        mLevelButtonDifference = (maxBtnWidth - minBtnWidth) / (NUM_LEVEL - 2)
-        mLevelButtonMargin = (mScreenHeight - 2 * mPaintStroke) / (NUM_LEVEL * 2 + NUM_LEVEL - 1)
+        mLevelButtonDifference = (maxBtnWidth - minBtnWidth) / (Companion.NUM_LEVEL - 2)
+        mLevelButtonMargin =
+            (mScreenHeight - 2 * mPaintStroke) / (Companion.NUM_LEVEL * 2 + Companion.NUM_LEVEL - 1)
         mLevelButtonHeight = mLevelButtonMargin * 2
         mPaintStroke = mLevelButtonHeight / 9
 
         mExtractLevels.clear()
-        for (i in 0 until NUM_LEVEL) {
+        for (i in 0 until Companion.NUM_LEVEL) {
             val width = when (i) {
                 0 -> mScreenWidth.toFloat() - 2 * mPaintStroke
-                else -> mExtractLevels[i-1].position.width() - mLevelButtonDifference
+                else -> mExtractLevels[i - 1].position.width() - mLevelButtonDifference
             }
 
             val left = (mScreenWidth - width) / 2
@@ -148,11 +151,11 @@ class MeterView @JvmOverloads constructor(
 
             MotionEvent.ACTION_MOVE -> {
                 val dy = y - mPreviousY
-                if (abs(dy) > SWIPE_MINIMUM_DISTANCE) {
+                if (abs(dy) > Companion.SWIPE_MINIMUM_DISTANCE) {
                     if (dy > 0) {
-                        mSwipeState = SWIPE_STATE.DOWN
+                        mSwipeState = SwipeState.DOWN
                     } else if (dy < 0) {
-                        mSwipeState = SWIPE_STATE.UP
+                        mSwipeState = SwipeState.UP
                     }
                 }
 
@@ -163,13 +166,13 @@ class MeterView @JvmOverloads constructor(
             }
 
             MotionEvent.ACTION_UP -> {
-                if (mSwipeState == SWIPE_STATE.NONE) {
+                if (mSwipeState == SwipeState.NONE) {
                     if (mCurrentLevel != currentTouchId && currentTouchId != -1) {
                         updateLevelState(currentTouchId)
                         invalidate()
                     }
                 }
-                mSwipeState = SWIPE_STATE.NONE
+                mSwipeState = SwipeState.NONE
                 mPreviousY = 0f
             }
         }
@@ -184,7 +187,7 @@ class MeterView @JvmOverloads constructor(
     }
 
     private fun changeLevelWithVibration(levelIndex: Int) {
-        vibrator.vibrate(VibrationEffect.createOneShot(TIMER_VIBRATION, -1))
+        vibrator.vibrate(VibrationEffect.createOneShot(Companion.TIMER_VIBRATION, -1))
         mCurrentLevel = levelIndex
     }
 
